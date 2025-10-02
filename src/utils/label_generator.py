62a2295897c2a36a93f3424ee7f0a9e63f6a9129
Your Name <kalcicova.andrea@gmail.com>
2025-10-02 15:53:49 +0200

    Fix label logging issue: remove delivery_date parameter that doesn't exist in LabelLog model

--- src/utils/label_generator.py
+++ src/utils/label_generator.py
@@ -561,13 +561,6 @@ class LabelGenerator:
                         # We'll set it to None and handle it in the database
                         order_item_id = None
                     
-                    # Get delivery date - try direct access first, then getattr as fallback
-                    delivery_date = None
-                    if hasattr(order_item, 'delivery_date'):
-                        delivery_date = order_item.delivery_date
-                    else:
-                        delivery_date = getattr(order_item, 'delivery_date', None)
-                    
                     label_log = LabelLog(
                         order_item_id=order_item_id,
                         customer_name=customer_name,
@@ -577,7 +570,6 @@ class LabelGenerator:
                         item_name=item_name,
                         quantity=box_qty,
                         printed_quantity=1,  # Each box gets one label
-                        delivery_date=delivery_date,
                         barcodes_included=barcodes_included,
                         item_barcode=item_barcode,
                         order_barcode=order_barcode,
